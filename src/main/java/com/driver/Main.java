62269532aefe85b854efa3dc1e242122891ab2f7
Javed Ansari <ansarizawed4@gmail.com>
2023-10-06 11:09:54 +0530

    final commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,13 +1,12 @@
 package com.driver;
-class A {
-    public String meth() {
-        return "Invoking method from class A";
+class A{
+    public String meth(){
+        return "Method is invoking from class A";
     }
 }
-class B extends A {
+class B extends A{
     @Override
-    public String meth() {
-
+    public String meth(){
         return "Method is overridden in Extendend class B";
     }
 }
